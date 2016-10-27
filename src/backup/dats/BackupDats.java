d2118cb14ae6762045e3ec48ec240dedeb3fd538
fernando <fernando@suporte112.makito.local>
2016-10-27 08:14:14 -0200

    Versão 1.0.0

---
+++ src/backup/dats/BackupDats.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package backup.dats;
+
+import java.awt.Image;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import sun.swing.BakedArrayList;
+
+/**
+ *
+ * @author fernando
+ */
+public class BackupDats {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        try {
+            // Set System L&F
+            //UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+        } catch (UnsupportedLookAndFeelException | ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+            // handle exception
+        }
+
+        FrmInicio frm = new FrmInicio();
+        frm.setIconImage(ImageIO.read(BackupDats.class.getResource("res/icon.png")));
+        frm.setVisible(true);
+    }
+
+}
